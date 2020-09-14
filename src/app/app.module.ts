11b3084c0698bc6bd3d9228c0ba001cb6f081bb8
Miguel Yann <tsambamiguelito@gmail.com>
2020-09-14 21:52:47 +0200

    (UI) Display star component for rating of products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { StarComponent } from './shared/stars/star.component';
 import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
 import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacePipe],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacePipe,
+    StarComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-
-}
+export class AppModule {}
