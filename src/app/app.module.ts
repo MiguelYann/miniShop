581e5ccf30b17d10deace06a8476d3c4303baeb8
Miguel Yann <tsambamiguelito@gmail.com>
2020-09-14 13:15:52 +0200

    * Pipe to transform data price * Filter product by his name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
 import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,7 +7,7 @@ import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,5 +1,5 @@
 export interface IProduct {
-  id: number;
+  code: string;
   productName: string;
   releaseDate: string;
   price: number,
